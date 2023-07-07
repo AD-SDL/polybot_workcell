144607163964275018955b93d2d32fb20f75991e
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-07-07 15:09:24 -0500

    refactor

--- polybot_app/demo_app.py
+++ polybot_app/demo_app.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
-import logging
 from pathlib import Path
-from argparse import ArgumentParser
 from rpl_wei import Experiment
 from pathlib import Path
 
-def main():
-    wf_path = Path('/home/rpl/workspace/polybot_workcell/polybot_workcell/workflows/demo.yaml')
 
-    exp = Experiment("127.0.0.1","8000","CNM Experiment")
+def main():
+    wf_path = Path(
+        "/home/rpl/workspace/polybot_workcell/polybot_app/workflows/demo.yaml"
+    )
+
+    exp = Experiment("127.0.0.1", "8000", "CNM Experiment")
     exp.register_exp()
 
-    flow_info = exp.run_job(wf_path.resolve(), simulate = False)
+    flow_info = exp.run_job(wf_path.resolve(), simulate=False)
     print(flow_info)
-    flow_status =exp.query_job(flow_info['job_id'])
+    flow_status = exp.query_job(flow_info["job_id"])
     print(flow_status)
 
     while flow_status["status"] != "finished":
@@ -26,6 +27,7 @@ def main():
 
     # run_info = wei_client.run_workflow(payload=payload)
     # print(run_info)
+
 
 if __name__ == "__main__":
     main()
