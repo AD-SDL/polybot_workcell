04b71efeba2ec1902d2eac62672d54f3c56fca86
Dozgulbas <dozgulbas@anl.gov>
2023-07-10 16:44:55 -0500

    Update

--- polybot_app/tools/flow_tecan.py
+++ polybot_app/tools/flow_tecan.py
@@ -18,35 +18,37 @@ class C2Flow(GladierBaseClient):
        'gladier_tools.publish.Publishv2'
     ]
 
-def tecan_flow(exp_name,plate_n,time, local_path, file_name):
-        experiment_label = "ECP_demo_batch_1"
+def tecan_flow(exp_name, plate_n, time, local_path, fname):
+        experiment_label = exp_name + "_" + plate_n + "_" + time
         remote_folder = os.path.join("C:\Users\cnmuser\Desktop\Polybot\tecan_code\uv_vis_data", experiment_label)
         local_funcx = 'NONE' #TODO: FIND FUNCX ID
         flow_input = {
             'input': {
                 'transfer_source_endpoint_id':'829262a0-1f5b-11ee-abf2-63e0d97254cd', #Tecan endpoint
-                'transfer_source_path': os.path.join(local_path, file_name), # Tecan file location
+                'transfer_source_path': os.path.join(local_path, fname), # Tecan file location
                 'transfer_destination_endpoint_id':'2204acb8-1f65-11ee-abf2-63e0d97254cd', #Batman enpoint
-                'trasnfer_destination_path': os.path.join(remote_folder, file_name),
+                'trasnfer_destination_path': os.path.join(remote_folder, fname),
                 'transfer_reqursive': False,
                 'funcx_endpoint_compute': local_funcx, #Batman funcx
                 'funcx_endpoint_non_compute': local_funcx, #Batman funcx
                 'exp_name':exp_name,
                 'plate_n':plate_n,
-                'make_input': local_path,
-                'local_path': local_path,
-                'remote_file': file_name,
-                'csv_file': file_name.split('.')[0] +".csv",
-                'csv_file_corr': file_name +"_corr.csv",
                 'proc_folder':remote_folder,
-                'time':time,
+                'file_name': fname,
+                'csv_file': fname.split('.')[0] +".asc",
+                'csv_file_corr': fname +"_corr.asc",
+                'time': time,
+                # 'make_input': local_path,
+                # 'local_path': local_path,
+                # 'remote_file': fname,
+
                 'publishv2': {
                     'dataset': remote_folder,
                     'index': '4e2884b0-e585-4913-8a33-4be155ebb06c',
-                    'project': 'bio',
-                    'source_collection': '1a11369a-d3eb-11ed-a9ce-63ca5f6c6821',
+                    'project': 'cnm',
+                    'source_collection': '829262a0-1f5b-11ee-abf2-63e0d97254cd',
                     'source_collection_basepath': '/',
-                    'destination_collection': 'bb8d048a-2cad-4029-a9c7-671ec5d1f84d',
+                    'destination_collection': '2204acb8-1f65-11ee-abf2-63e0d97254cd',
                     'metadata': {},
                     'ingest_enabled': True,
                     'transfer_enabled':True,
@@ -76,7 +78,8 @@ def tecan_flow(exp_name,plate_n,time, local_path, file_name):
      
         
 if __name__ == "__main__":
-  local_path = "/home/tginsbu/workspace/rpl_workcell/bio_workcell/demo_data/example_report"
-  file_name = "Campaign1_noIncubate2_20221201_160907.xlsx"
+  local_path = "/home/rpl/workspace/polybot_workcell/demo_data/ECP_demo_batch_1.asc"
+  fname = "ECP_demo_batch_1.asc"
+  exp_name = "first_exp"
 
-  tecan_flow("test_exp", 1, "time", local_path, file_name)
+  tecan_flow(exp_name = exp_name, plate_n = 1, time = "time", local_path = local_path, fname = fname)
