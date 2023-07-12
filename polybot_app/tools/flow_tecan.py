e0f8759049a35b9e3d41f051e1ec8dfd7858b077
Dozgulbas <dozgulbas@anl.gov>
2023-07-12 16:48:32 -0500

    Fixed errors

--- polybot_app/tools/flow_tecan.py
+++ polybot_app/tools/flow_tecan.py
@@ -1,86 +1,65 @@
 from gladier import GladierBaseClient, generate_flow_definition
 
 from tecan_read import Tecan_Read
-from tecan_proc import Tecan_Proc
-from gather_data import GatherMetaData
-from model_update import Update_Model
+# from tecan_proc import Tecan_Proc
+# from gather_data import GatherMetaData
+# from model_update import Update_Model
 import os
 
-@generate_flow_definition(modifiers={'publishv2_gather_metadata' : {'payload': '$.GatherMetadata.details.result[0]'}})
-class C2Flow(GladierBaseClient):
+@generate_flow_definition()#modifiers={'publishv2_gather_metadata' : {'payload': '$.GatherMetadata.details.result[0]'}})
+class polybot_Flow(GladierBaseClient):
     globus_group = 'dda56f31-53d1-11ed-bd8b-0db7472df7d6'
     gladier_tools = [
-       'gladier_tools.transfer.Transfer',
-        Tecan_Read,
-        Tecan_Proc,
+       'gladier_tools.globus.transfer.Transfer',
+       Tecan_Read,
+        # Tecan_Proc,
         #Update_Model,
-        GatherMetaData,
-       'gladier_tools.publish.Publishv2'
+    #     GatherMetaData,
+    #    'gladier_tools.publish.Publishv2'
     ]
 
-def tecan_flow(exp_name, plate_n, time, local_path, fname):
-        experiment_label = exp_name + "_" + plate_n + "_" + time
-        remote_folder = os.path.join("C:\Users\cnmuser\Desktop\Polybot\tecan_code\uv_vis_data", experiment_label)
-        local_funcx = 'NONE' #TODO: FIND FUNCX ID
+def tecan_flow( local_path, fname):
+        remote_folder = "/C/Users/cnmuser/Desktop/Polybot/tecan_code/uv_vis_data"
+        globus_compute_local = 'f352ef00-268a-4552-8a57-4772f51b54e4'
         flow_input = {
             'input': {
-                'transfer_source_endpoint_id':'829262a0-1f5b-11ee-abf2-63e0d97254cd', #Tecan endpoint
-                'transfer_source_path': os.path.join(local_path, fname), # Tecan file location
-                'transfer_destination_endpoint_id':'2204acb8-1f65-11ee-abf2-63e0d97254cd', #Batman enpoint
-                'trasnfer_destination_path': os.path.join(remote_folder, fname),
-                'transfer_reqursive': False,
-                'funcx_endpoint_compute': local_funcx, #Batman funcx
-                'funcx_endpoint_non_compute': local_funcx, #Batman funcx
-                'exp_name':exp_name,
-                'plate_n':plate_n,
-                'proc_folder':remote_folder,
+                'transfer_source_endpoint_id':'829262a0-1f5b-11ee-abf2-63e0d97254cd', #Tecan Transfer endpoint
+                'transfer_source_path': os.path.join(remote_folder, fname), # Tecan file location
+                'transfer_destination_endpoint_id':'2204acb8-1f65-11ee-abf2-63e0d97254cd', #Batman Transfer endpoint
+                'transfer_destination_path': os.path.join(local_path, fname),
+                'transfer_recursive': False,
+                'compute_endpoint': globus_compute_local, #Batman compute endpoint
+                'proc_folder': local_path,
                 'file_name': fname,
-                'csv_file': fname.split('.')[0] +".asc",
-                'csv_file_corr': fname +"_corr.asc",
-                'time': time,
-                # 'make_input': local_path,
-                # 'local_path': local_path,
-                # 'remote_file': fname,
-
-                'publishv2': {
-                    'dataset': remote_folder,
-                    'index': '4e2884b0-e585-4913-8a33-4be155ebb06c',
-                    'project': 'cnm',
-                    'source_collection': '829262a0-1f5b-11ee-abf2-63e0d97254cd',
-                    'source_collection_basepath': '/',
-                    'destination_collection': '2204acb8-1f65-11ee-abf2-63e0d97254cd',
-                    'metadata': {},
-                    'ingest_enabled': True,
-                    'transfer_enabled':True,
-                    'destination':str("/portal/cnm"),
-                    'visible_to' : ['public']
-                   }
-                # 'pilot': {
-                #     'dataset': str(folder_path.expanduser()),
+                # 'publishv2': {
+                #     'dataset': remote_folder,
                 #     'index': '4e2884b0-e585-4913-8a33-4be155ebb06c',
-                #     'project': 'bio',
-                #     'transfer_source_endpoint_id': '95038e17-339b-4462-9c9f-a8473809af25',
+                #     'project': 'cnm',
+                #     'source_collection': '829262a0-1f5b-11ee-abf2-63e0d97254cd',
                 #     'source_collection_basepath': '/',
+                #     'destination_collection': '2204acb8-1f65-11ee-abf2-63e0d97254cd',
                 #     'metadata': {},
-                #     'destination':str(dest_path)
+                #     'ingest_enabled': True,
+                #     'transfer_enabled':True,
+                #     'destination':str("/portal/cnm"),
+                #     'visible_to' : ['public']
                 #    }
                 }
             }
 
         # Create the Client
-        publishFlow = C2Flow()
-        label = 'BioTestFlow'
+        publishFlow = polybot_Flow()
+        label = 'PolybotTestFlow'
         # Run the flow
+        print(flow_input)
         flow = publishFlow.run_flow(flow_input=flow_input,label=label)
         # Track progress
-        # action_id = flow['action_id']
-        # publishFlow.progress(action_id)
+        action_id = flow['action_id']
+        publishFlow.progress(action_id)
      
         
 if __name__ == "__main__":
-  local_path = "/home/rpl/workspace/polybot_workcell/demo_data/ECP_demo_batch_1.asc"
+  local_path = "/home/rpl/workspace/polybot_workcell/demo_data/"
   fname = "ECP_demo_batch_1.asc"
-  exp_name = "first_exp"
-  
 
-  tecan_flow(exp_name = exp_name, plate_n = 1, time = "time", local_path = local_path, fname = fname)
+  tecan_flow(local_path = local_path, fname = fname)
